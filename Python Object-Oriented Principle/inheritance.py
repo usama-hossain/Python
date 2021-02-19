3d8849a5b646d542c9ab7dcab13aa12721c04d7e
Usama Hossain <scarredentos@gmail.com>
2021-02-19 22:53:56 +0600

    revisited notebook problem

--- Python Object-Oriented Principle/inheritance.py
+++ Python Object-Oriented Principle/inheritance.py
@@ -1,3 +1,12 @@
+class AddressHolder:
+
+    def __init__(self, street, city, state, code):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.code = code
+
+
 class ContactList(list):
 
     def search(self, name):
--- Python Object-Oriented Principle/notebook/notebook.py
+++ Python Object-Oriented Principle/notebook/notebook.py
@@ -28,20 +28,27 @@ class Notebook:
     def __init__(self):
         self.notes = []
 
+    def _find_note(self, note_id):
+        """Locate note with given note_id
+
+        Args:
+            note_id (int): id of our note
+        """
+        for note in self.notes:
+            if note.id == note_id:
+                return note
+            return None
+
     def new_note(self, memo, tag=''):
         self.notes.append(Note(memo, tag))
 
     def modify_memo(self, note_id, memo):
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        """Find the note with the given id and replace memo with given memo"""
+        self._find_note(note_id).memo = memo
 
     def modify_tags(self, note_id, tag):
-        for note in self.notes:
-            if note.id == note_id:
-                note.tag = tag
-                break
+
+        self._find_note(note_id).tag = tag
 
     def search(self, filter):
         return [note for note in self.notes if note.match(filter)]
