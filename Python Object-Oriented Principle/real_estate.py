8bb7c4e66822273c2964b2c051d6e2ade2742c40
Usama Hossain <mohammed.usama@northsouth.edu>
2021-03-14 23:58:46 +0600

    Working with objects: when they are alike

---
+++ Python Object-Oriented Principle/real_estate.py
@@ -0,0 +1,10 @@
+class Property:
+    def __init__(self, square_feet='', beds='', baths='', **kwargs):
+        super().__init__(**kwargs)
+        self.square_feet = square_feet
+        self.num_bedrooms = beds
+        self.num_baths = baths
+        
+    def display(self):
+        print("Property Details!!!")
+        print("===================")
